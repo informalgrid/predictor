59e6c956e713473d33ac3ffbbbe84726f7fd8823
Om Sawant <sawantom985@gmail.com>
2026-04-13 19:22:52 +0530

    fixed POST method error

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request
-import pickle
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open("model.pkl", "rb"))
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    if request.method == "POST":
+        hours = float(request.form["hours"])
+        prediction = model.predict([[hours]])
+        return render_template("index.html", prediction=round(prediction[0],2))
+    
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    hours = float(request.form['hours'])
-    prediction = model.predict([[hours]])
-    return render_template('index.html', result=prediction[0])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-
-
